0e59cba31e318be96bcaaedb51243ee02a15b5de
Wen Bin <wenbin_ng@yahoo.com.sg>
2020-09-06 23:42:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1164,14 +1164,14 @@ def main():
     dp.add_error_handler(error)
 
     # For production deployment
-    # updater.start_webhook(listen="0.0.0.0",
-    #                       port=int(PORT),
-    #                       url_path=TOKEN)
-    # updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
 
     # # For local hosting ONLY
-    updater.start_polling()
-    updater.idle()
+    # updater.start_polling()
+    # updater.idle()
 
 if __name__ == '__main__':
     main()
