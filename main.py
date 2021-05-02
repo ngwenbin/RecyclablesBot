469c9a7051d37555592074788135887c502c57ae
Jason <jasontay9645@gmail.com>
2021-05-02 12:57:14 +0800

    Standardise variable names

--- main.py
+++ main.py
@@ -731,41 +731,71 @@ def check_past_orders(update, context):
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     now = datetime.now()
-    keyboard_button = []
-
-    orders_collection_ref = db.collection(u'orders').where(u'userid', u'==', userids).stream()
-    # orders_collection_ref = db.collection(u'orders').where(u'userid', u'==', '1356291238').stream()
-    orders_list = []
+    # orders_collection_ref = db.collection(u'orders').where(u'userid', u'==', userids).stream()
+    orders_collection_ref = db.collection(u'orders').where(u'userid', u'==', '1356291238').stream()
+    past_orders_list = []
 
     for order in orders_collection_ref:
         timeslot = order.to_dict().get("timeslot")
-        order_time = timeslot[-11:-1] # get order time in form of dd/mm/yyyy
+        order_time = timeslot[-11:-1] #  get order time in form of dd/mm/yyyy
         order_datetime = datetime(int(order_time[6:10]), int(order_time[3:5]), int(order_time[:2]))
         if (order_datetime < now):
-            orders_list.append(order)
+            past_orders_list.append(order)
 
-    num_of_orders = len(orders_list)
-    ordersString = ''
-    i = 1
-    for order in orders_list:
-        orderno = "\nOrder " + str(order.to_dict().get("ordernum"))
-        strings = [orderno + "\n==================", "*Recyclables:*\n" + f'{order.to_dict().get("item")}', "\n*Date:*", f'{order.to_dict().get("timeslot")}\n']
-        ordersString = ordersString + '\n'.join(strings)
-        i += 1
-    update.callback_query.answer()
+    num_of_orders = len(past_orders_list)
     if num_of_orders == 0:
+        update.callback_query.answer()
         update.callback_query.edit_message_text(
             text="You don't have any past orders.\n",
             reply_markup=reply_markup,
             parse_mode='Markdown',
         )
     else:
+        past_orders_list.reverse()  # so earliest order in front
+        orders_string = ''
+        i = 1
+        for order in past_orders_list:
+            # orderno = "\nOrder " + str(order.to_dict().get("ordernum"))
+            orderno = "\nOrder " + str(i)
+            strings = [orderno + "\n==================", "*Recyclables:*\n" + f'{order.to_dict().get("item")}', "\n*Date:*", f'{order.to_dict().get("timeslot")}\n']
+            orders_string = orders_string + '\n'.join(strings)
+            # load_new_page(i, order_string, num_of_orders)
+            i += 1
+        update.callback_query.answer()
         update.callback_query.edit_message_text(
-            text="Here are your past orders:\n" + ordersString,
+            text="Here are your past orders:\n" + orders_string,
             reply_markup=reply_markup,
             parse_mode='Markdown',
         )
     return MY_ORDERS
+
+
+# def load_new_page(ordernum, orderinfo, listsize):
+#     keyboard = [[InlineKeyboardButton("« Back", callback_data=str(END))]]
+
+#     if ordernum == 1:
+#         keyboard = keyboard + [InlineKeyboardButton("Next order", callback_data=str(CHECK_ORDERS))]
+#     elif ordernum > 1 and ordernum < listsize:
+#         keyboard = keyboard + [InlineKeyboardButton("Previous order", callback_data=str(CHECK_ORDERS))]
+#         keyboard = keyboard + [InlineKeyboardButton("Next order", callback_data=str(CHECK_ORDERS))]
+#     elif ordernum == listsize:
+#         keyboard = keyboard + [InlineKeyboardButton("Previous order", callback_data=str(CHECK_ORDERS))]
+#     else:
+#         update.callback_query.edit_message_text(
+#             text="Error at load past order, please restart bot\n",
+#             reply_markup=reply_markup,
+#             parse_mode='Markdown',
+#         )
+
+#     reply_markup = InlineKeyboardMarkup(keyboard)
+
+#     update.callback_query.answer()
+#     update.callback_query.edit_message_text(
+#         text="Hi, what would you like to do?",
+#         reply_markup=reply_markup
+#     )
+#     return MY_ORDERS
+
 
 def orders_to_cancel(update, context):
     userids = str(update.effective_user.id)
